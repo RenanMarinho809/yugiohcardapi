54e4ed129bffefac73d78f5cf558dd0495b8d0f3
RenanMarinho809 <renanmarinho809@gmail.com>
2025-01-30 18:38:51 -0300

    refactor: remove unused files and clean up codebase

--- controllers/listallcard.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import monsterDuel from "../cards/card-monsterduel";
-import { HttpStatuscode } from "../utils/http-server";
-
-
-
-export const listAllCards = (req: Request, res: Response) => {
-    res.status(HttpStatuscode.OK).json(monsterDuel);
-}
--- data/deck.ts
+++
@@ -1,7 +0,0 @@
-export type Tcard = {
-    card_id : number,
-    card_name : string,
-    level : number,
-    atk : number,
-    def : number
-}
