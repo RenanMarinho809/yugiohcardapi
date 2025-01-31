b76be55eda525d3ba41f280c376ea01e448d0880
RenanMarinho809 <renanmarinho809@gmail.com>
2025-01-31 20:31:50 -0300

    feat: add getMonsterByID controller and route for retrieving monster cards by ID

--- src/controllers/getMonsterByID.ts
+++ src/controllers/getMonsterByID.ts
@@ -1,10 +1,17 @@
-import express, { Request, Response } from 'express';
+import { Request, Response } from 'express';
 import monsterDuel from '../cards/card-monsterduel';
 import { HttpStatuscode } from '../utils/http-server';
 
 
 const getMonsterByID = (req: Request, res: Response) => {
-    const { id } = req.params;
-    const monster = monsterDuel.find((card) => card.card_id === Number(id));
-    return res.json(monster).status(HttpStatuscode.OK);
+    const id = req.params.id;
+    const monsterDuelByID = monsterDuel.find((card) => card.card_id === Number(id));
+    
+    if (!monsterDuelByID) 
+     res.status(HttpStatuscode.NotFound).json({ message: 'Card not found' });
+        else
+     res.status(HttpStatuscode.OK).json(monsterDuelByID);
+    
 }
+
+export default getMonsterByID;
--- src/routes/rotas.ts
+++ src/routes/rotas.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import getCardList from "../controllers/getCardList";
+import getMonsterByID from "../controllers/getMonsterByID";
+
+
+
+
+
 
 
 const rotas = Router();
 
 rotas.get('/cards', getCardList);
+rotas.get('/cards/:id', getMonsterByID);
 
 export default rotas;
